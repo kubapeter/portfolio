67b5ee52f6d00d65eabfb5a464d8acd85d2818a1
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-06 12:10:24 +0200

    Create lab3.sql

---
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab3.sql
@@ -0,0 +1,15 @@
+--2.1 List the employeeid, firstname + lastname concatenated as ‘employee’, and the age of the employee  when they were hired. 
+
+SELECT employeeid, firstname || ' ' || lastname AS "employee",
+    age(hiredate, birthdate)::text
+  FROM "alanparadise/nw"."employees"
+
+--2.2 Run a query to calculate your age as of today.  
+
+SELECT age(now(), '19770701')::text
+
+--2.3 List the customerid, companyname and country for all customers whose region is NULL.    
+
+SELECT customerid, companyname, country 
+  FROM "alanparadise/nw"."customers"
+  WHERE region IS NULL
