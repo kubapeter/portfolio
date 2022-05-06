9bcbbe7b70ff8cc5b2916e6ca267fb5bdb82d4b6
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-06 10:45:25 +0200

    Create lab2.sql

---
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab2.sql
@@ -0,0 +1,9 @@
+--2.1 List the customerid, companyname, and country for all customers NOT in the U.S.A.
+
+--2.2 For all products in the Northwinds database, list the productname, unitprice, unitsinstock,  and the total value of the inventory of that product as “Total Value” for all products with a Total Value greater than $100.  (HINT:  total value = unitsinstock * unitprice) 
+
+--2.3 List the productid, productname, and quantityperunit for all products that come in bottles.   
+
+--2.4 List the productid, productname, and unitprice for all products whose categoryid is an ODD number.   (HINT:  categoryid is a one digit integer less than 9 …) 
+
+--2.5 List the orderid,  customerid, and shippeddate for orders that shipped to Canada  in December 1996 through the end of  January 1997.
