a670245bf51a776655bb40b5decf75dfbe1151b2
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-06 12:15:04 +0200

    Update lab3.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab3.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab3.sql
@@ -5,9 +5,19 @@ SELECT employeeid, firstname || ' ' || lastname AS "employee",
     age(hiredate, birthdate)::text
   FROM "alanparadise/nw"."employees"
 
+--or
+
+SELECT EmployeeID, Firstname || ' ' || Lastname as "employee",
+		cast (age(HireDate, BirthDate)as text) AS HIRE_AGE 
+   	FROM "alanparadise/nw"."employees";
+
 --2.2 Run a query to calculate your age as of today.  
 
 SELECT age(now(), '19770701')::text
+
+--or
+
+SELECT CAST(age(now(),'2010-09-08')AS text);
 
 --2.3 List the customerid, companyname and country 
 --for all customers whose region is NULL.    
