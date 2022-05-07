cb4d62e3de521aaa1c7db77544e0bd0cdf5777e9
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-07 18:57:08 +0200

    Update lab5.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab5.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab5.sql
@@ -3,10 +3,11 @@
 
 SELECT productid, productname, unitprice
   FROM "alanparadise/nw"."products"
-  WHERE unitprice = (
-    SELECT MIN(unitprice)
-    FROM "alanparadise/nw"."products"
-  )
+  WHERE unitprice = 
+    (
+      SELECT MIN(unitprice)
+      FROM "alanparadise/nw"."products"
+    )
 
 --actually does not need subquery:
 
@@ -19,11 +20,43 @@ SELECT productid, productname, unitprice
 
 SELECT COUNT(orderid)
   FROM "alanparadise/nw"."orders"
-  WHERE customerid NOT IN (
-    SELECT customerID 
-      FROM "alanparadise/nw"."customers"
-  )
+  WHERE customerid NOT IN 
+    (
+      SELECT customerID 
+        FROM "alanparadise/nw"."customers"
+    )
 
---2.3 Use a subquery in a SELECT to list something interesting.   
+--2.3 Use a subquery in a SELECT to list something interesting.  
+
+SELECT productname, 
+    (
+      SELECT to_char(sum(Unitprice*Unitsinstock),'999,999.99') AS TotalValue 
+        FROM "alanparadise/nw"."products"
+    )
+  FROM "alanparadise/nw"."products";   
+  
+--(total value of each product stocked by the company)
+
 
 --2.4 Use a subquery in a FROM to list something interesting.  
+
+SELECT 
+    orderid,
+    ROUND(CAST(sum(total_cost) AS numeric),2) total_value,
+    TRUNC(sum(discount)*100) total_discounts
+FROM
+  (
+      SELECT  orderid, (quantity*unitprice) total_cost, discount
+        FROM "alanparadise/nw"."orderdetails"
+        WHERE discount IN 
+          (
+            SELECT discount 
+              FROM "alanparadise/nw"."orderdetails"
+              GROUP BY discount
+              ORDER BY 1 DESC LIMIT 2
+          )
+  ) AS a
+GROUP BY orderid
+ORDER BY 3 DESC;
+
+--orders with large amount of discounts
