b5c12177a97587b76f9c938f62e3715cadb6cc9f
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-07 11:44:21 +0200

    Update lab4.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab4.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab4.sql
@@ -1,26 +1,34 @@
 --2.1 List the total (unitprice * quantity) as “Total Value”  by orderid for the top 5 orders.  (That is, the five orders with the highest Total Value.)  
 
-SELECT orderid, (unitprice * quantity) as "Total Value"
-  FROM "alanparadise/nw"."orderdetails"
-  ORDER BY (unitprice * quantity) DESC
+SELECT OrderID, 
+		SUM(UnitPrice * Quantity) AS "Total Value" 
+	    FROM "alanparadise/nw"."orderdetails"
+	GROUP BY OrderID
+	ORDER BY 2 DESC LIMIT 5;
 
 --2.2 How many products does Northwinds have in inventory?    
 
-SELECT SUM(unitsinstock) 
-  FROM "alanparadise/nw"."products";
+SELECT count(productid) 
+	FROM "alanparadise/nw"."products"
+	WHERE unitsinstock > 0;
 
 --2.3 How many products are out of stock? 
+
+SELECT count(productid) 
+	FROM "alanparadise/nw"."products"
+  WHERE unitsinstock = 0;
 
 --2.4 From which supplier(s) does Northwinds carry the fewest products? 
 
 SELECT supplierid, count(productid) 
-  FROM "alanparadise/nw"."products"
+    FROM "alanparadise/nw"."products"
   GROUP BY supplierid
   ORDER BY count(productid) 
 
 --2.5 Which Northwinds employees (just show their employeeid) had over 100 orders ?  
 
-SELECT employeeid, COUNT(orderid)
-  FROM "alanparadise/nw"."orders"
+SELECT employeeid, count(orderid) AS "Orders" 
+    FROM "alanparadise/nw"."orders"
   GROUP BY employeeid
-  HAVING COUNT(orderid) > 100
+  HAVING count(orderid) > 100
+  ORDER BY 2 desc;
