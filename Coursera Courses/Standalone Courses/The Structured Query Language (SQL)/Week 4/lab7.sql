e3ef52f6446cbc8455b33fd88a807a9b26ab6f3f
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-08 09:43:23 +0200

    Create lab7.sql

---
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 4/lab7.sql
@@ -0,0 +1,7 @@
+--2.1 Are there any Northwinds employees that have no orders?    
+
+--2.2 Are there any Northwinds customers that have no orders?     
+
+--2.3 Are there any Northwinds orders that have bad (not on file) customer numbers? 
+
+--2.4 Are there any Shippers that have shipped no Northwinds orders?  
