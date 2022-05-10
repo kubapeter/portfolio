44e62401b72799e89fca28e73082cf014884ab33
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-10 09:40:35 +0200

    Create lab11.sql

---
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 6/lab11.sql
@@ -0,0 +1,20 @@
+--1. Create a View
+
+CREATE VIEW "your_bit.io_account/demo_repo"."TopCustomers" AS 
+  SELECT companyname, sum(unitprice * quantity) AS "Total Sales"
+    FROM 
+      "alanparadise/nw"."customers" C 
+      JOIN
+      "alanparadise/nw"."orders" O 
+      ON 
+      C.customerid  =  O.customerid 
+      JOIN 
+      "alanparadise/nw"."orderdetails" D 
+      ON O.orderid  =  D.orderid
+  GROUP BY companyname 
+  ORDER BY 2 DESC LIMIT 5;
+
+--2. Run a Query Against Your View
+
+SELECT * 
+  FROM "your_bit.io_account/demo_repo"."TopCustomers"
