5834b4f0f8725062a0fc95d5adc303075d831be9
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-11 19:11:25 +0200

    Update lab4.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab4.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab4.sql
@@ -10,26 +10,26 @@ SELECT OrderID, SUM(UnitPrice * Quantity) AS "Total Value"
 --2.2 How many products does Northwinds have in inventory?    
 
 SELECT count(productid) 
-	FROM "alanparadise/nw"."products"
-	WHERE unitsinstock > 0;
+  FROM "alanparadise/nw"."products"
+  WHERE unitsinstock > 0;
 
 --2.3 How many products are out of stock? 
 
 SELECT count(productid) 
-	FROM "alanparadise/nw"."products"
+  FROM "alanparadise/nw"."products"
   WHERE unitsinstock = 0;
 
 --2.4 From which supplier(s) does Northwinds carry the fewest products? 
 
 SELECT supplierid, count(productid) 
-    FROM "alanparadise/nw"."products"
+  FROM "alanparadise/nw"."products"
   GROUP BY supplierid
   ORDER BY count(productid) 
 
 --2.5 Which Northwinds employees (just show their employeeid) had over 100 orders ?  
 
 SELECT employeeid, count(orderid) AS "Orders" 
-    FROM "alanparadise/nw"."orders"
+  FROM "alanparadise/nw"."orders"
   GROUP BY employeeid
   HAVING count(orderid) > 100
   ORDER BY 2 desc;
