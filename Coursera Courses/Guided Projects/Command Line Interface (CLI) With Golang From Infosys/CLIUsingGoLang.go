7f02808ff0ffeacf72d708415db5ea33ded394be
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-17 16:56:24 +0200

    Update CLIUsingGoLang.go

--- Coursera Courses/Guided Projects/Command Line Interface (CLI) With Golang From Infosys/CLIUsingGoLang.go
+++ Coursera Courses/Guided Projects/Command Line Interface (CLI) With Golang From Infosys/CLIUsingGoLang.go
@@ -9,5 +9,39 @@ import (
 )
 
 func main() {
-  
+	fmt.Println("Cloud CLI")
+	fmt.Println("---------------------------------")
+	fmt.Print("=>")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		fmt.Println(userInput)
+		fmt.Print("=>")
+		
+		if strings.Compare("exit", userInput) == 0 {
+			os.Exit(0)
+		} 
+
+		// var commandString[] string = strings.Split(userInput, " ")
+		var commandString []string = strings.Split(userInput, " ")
+
+		if strings.Compare("checkinfyconnect", commandString[0]) == 0 {
+			commandOutput := execPINGCommand(commandString[1])
+			fmt.Println(commandOutput)
+		} else {
+			fmt.Println("Command not supported by the CLI")
+		}
+		fmt.Print("=>")
+	}
+}
+
+func execPINGCommand(cmdParam string) string {
+	if c, err := exec.Command("cmd", "/c", "ping", "-n", "3", cmdParam).CombinedOutput();
+	err != nil {
+		errorMessage := "Error! Command execution failed"
+		return errorMessage
+	} else {
+		return string(c)
+	}
 }
