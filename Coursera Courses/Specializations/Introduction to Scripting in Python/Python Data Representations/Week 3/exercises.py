62fcc252fadbcce20e0627b4cb32c10e2bf22fc7
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-18 23:22:57 +0200

    Update exercises.py

--- Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 3/exercises.py
+++ Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 3/exercises.py
@@ -1,1 +1,15 @@
+Given a list example_list containing at least 4 elements, write an expression that assigns the value 0 to the third element of the list. List assign template --- List assign solution
 
+Given a list example_list containing at least 4 elements, write an expression that replaces the second and third elements of the list by the list [0, 0, 0] . Slice update template --- Slice update solution
+
+Given a list example_list such that the number 0 is appended (i.e. added to the end) to the list. Append template --- Append solution
+
+Given a list example_list such that the numbers in the list [0, 0, 0] are appended to the list. You should not use a loop here. Extend template --- Extend solution
+
+Given a list example_list, write an expression that appends the numbers in the list [0, 0, 0] to example_list such that the numbers in the list [0, 0, 0] are appended to the list. Loop append template --- Loop append solution
+
+Given a list example_list by shuffling (i.e. random module. Shuffle template --- Shuffle solution
+
+Challenge: The items in a list can themselves be lists. These nested lists can be used to represent a wide range of 2D data such as spreadsheet information. Write a function flatten(nested_list. The function flaten() should return a list consisting of the items in the sublists of nested_list all appended together. (See the provided template for example input and output.) Flatten template --- Flatten solution
+
+Challenge: Write a function remove_duplicates(items. Remove duplicates template --- Remove duplicates solution
