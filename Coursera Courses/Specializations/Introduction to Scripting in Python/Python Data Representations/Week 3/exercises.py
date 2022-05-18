7426e6ae308f451d6047c645ea466bc3e2cc1ae5
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-18 23:48:13 +0200

    Update exercises.py

--- Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 3/exercises.py
+++ Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 3/exercises.py
@@ -1,15 +1,53 @@
-Given a list example_list containing at least 4 elements, write an expression that assigns the value 0 to the third element of the list. List assign template --- List assign solution
+# 1. 
 
-Given a list example_list containing at least 4 elements, write an expression that replaces the second and third elements of the list by the list [0, 0, 0] . Slice update template --- Slice update solution
+# Given a list example_list containing at least 4 elements, write an expression that assigns the value 0 to the 
+# third element of the list. 
 
-Given a list example_list such that the number 0 is appended (i.e. added to the end) to the list. Append template --- Append solution
+# 2. 
 
-Given a list example_list such that the numbers in the list [0, 0, 0] are appended to the list. You should not use a loop here. Extend template --- Extend solution
+# Given a list example_list containing at least 4 elements, write an expression that replaces the second and 
+# third elements of the list by the list [0, 0, 0].
 
-Given a list example_list, write an expression that appends the numbers in the list [0, 0, 0] to example_list such that the numbers in the list [0, 0, 0] are appended to the list. Loop append template --- Loop append solution
+# 3. 
 
-Given a list example_list by shuffling (i.e. random module. Shuffle template --- Shuffle solution
+# Given a list example_list, write an expression that mutates example_list such that the number 0 is 
+# appended (i.e. added to the end) to the list. 
 
-Challenge: The items in a list can themselves be lists. These nested lists can be used to represent a wide range of 2D data such as spreadsheet information. Write a function flatten(nested_list. The function flaten() should return a list consisting of the items in the sublists of nested_list all appended together. (See the provided template for example input and output.) Flatten template --- Flatten solution
+# 4. 
 
-Challenge: Write a function remove_duplicates(items. Remove duplicates template --- Remove duplicates solution
+# Given a list example_list, write an expression that mutates example_list such that the numbers in the 
+# list [0, 0, 0] are appended to the list. You should not use a loop here. 
+
+# 5. 
+
+# Given a list example_list, write an expression that appends the numbers in the list [0, 0, 0] to 
+# example_list. Note that this expression should not mutate example_list. (Again, you should not use a 
+# loop here.) 
+
+# 6. 
+
+# Given a list example_list, write a loop that mutates example_list such that the numbers in the list 
+# [0, 0, 0] are appended to the list. 
+
+# 7. 
+
+# Given a list example_list, write an expression that constructs a tuple whose items correspond to those in 
+# example_list. 
+
+# 8. 
+
+# Given a list example_list, write an expression that mutates example_list by shuffling (i.e. randomly 
+# reordering) the items in this list. Hint: investigate the various methods in the random module.
+
+# 9. 
+
+# Challenge: The items in a list can themselves be lists. These nested lists can be used to represent a wide range of 
+# 2D data such as spreadsheet information. Write a function flatten(nested_list) that takes as input the 
+# list of lists nested_list. The function flaten() should return a list consisting of the items in the sublists of 
+# nested_list all appended together. (See the provided template for example input and output.)
+
+# 10. 
+
+# Challenge: Write a function remove_duplicates(items) that takes a list items and returns a new list 
+# that consists of all unique items in items. The items in the returned list should be in the same order as those in 
+# items. 
