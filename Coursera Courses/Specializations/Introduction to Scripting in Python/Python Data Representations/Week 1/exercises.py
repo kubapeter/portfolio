e4c76c4ad90c52bcf3839591c62569af04493cd7
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-18 11:05:07 +0200

    Update exercises.py

--- Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 1/exercises.py
+++ Coursera Courses/Specializations/Introduction to Scripting in Python/Python Data Representations/Week 1/exercises.py
@@ -1,1 +1,17 @@
-a
+The provided template gives four possible defintions for the string It's just a flesh wound\color{red}{\verb|It's just a flesh wound|}It’s just a flesh wound in Python. Determine which definitions are incorrect and fix them.
+
+"""
+Solution - String examples
+"""
+
+# Fix the four string definitions below
+
+string1 = "It's just a flesh wound"
+string2 = "It's just a flesh wound"	
+string3 = "It's just a flesh wound"	
+string4 = """It's just a flesh wound"""
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
