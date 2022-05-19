e26b209427f9e7707827b1f217fb8a24faff31d0
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-19 16:34:12 +0200

    Update exercise.sql

--- Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 1/exercise.sql
+++ Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 1/exercise.sql
@@ -1,1 +1,21 @@
--- Retrieve all the records from the Employees table.
+-- 1. Retrieve all the records from the Employees table.
+
+SELECT * 
+  FROM Employees
+
+-- 2. Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
+
+SELECT 
+    FirstName, 
+    LastName, 
+    Birthdate, 
+    Address, 
+    City, 
+    State 
+  FROM Employees
+  
+-- 3. Retrieve all the columns from the Tracks table, but only return 20 rows.
+
+SELECT * 
+  FROM Tracks 
+  LIMIT 20
