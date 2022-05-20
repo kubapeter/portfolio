fa5887ff94b6d54c5bef7da0dfcd82769cba0759
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-20 10:48:55 +0200

    Update exercise.sql

--- Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 3/exercise.sql
+++ Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 3/exercise.sql
@@ -1,1 +1,9 @@
 -- 1. How many albums does the artist Led Zeppelin have?
+
+-- 2. Create a list of album titles and the unit prices for the artist "Audioslave".
+
+-- 3. Find the first and last name of any customer who does not have an invoice. Are there any customers returned from the query?
+
+-- 4. Find the total price for each album.
+
+-- 5. How many records are created when you apply a Cartesian join to the invoice and invoice items table?
