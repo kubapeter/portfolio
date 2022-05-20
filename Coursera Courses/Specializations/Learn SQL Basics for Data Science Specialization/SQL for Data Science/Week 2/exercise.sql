d154a03677e9d5e39b7218414b06a7f5f7319fc6
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-20 10:23:15 +0200

    Update exercise.sql

--- Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 2/exercise.sql
+++ Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/SQL for Data Science/Week 2/exercise.sql
@@ -15,13 +15,92 @@
 --    Extended_Step 
 --    Pay_Type
     
--- 1. Find the distinct values for the extended step. 
--- The code has been started for you, but you will need to program the third line yourself before running the query.
 
-SELECT 
-    DISTINCT Extended_step
-FROM salary_range_by_job_classification;
+-- 2. Find the distinct values for the extended step. 
+
+SELECT DISTINCT Extended_step
+  FROM salary_range_by_job_classification
 
 -- Which of the following values is not a distinct value?
 
 5
+
+
+-- 3. Excluding $0.00, what is the minimum bi-weekly high rate of pay (please include the dollar sign and decimal point in your answer)? 
+
+SELECT MIN(Biweekly_high_Rate)
+  FROM salary_range_by_job_classification 
+  WHERE biweekly_high_rate <> '$0.00'
+
+
+-- 4. What is the maximum biweekly high rate of pay (please include the dollar sign and decimal point in your answer)?
+
+
+-- 5. What is the pay type for all the job codes that start with '03'?
+
+SELECT job_code, pay_type
+  FROM salary_range_by_job_classification
+  WHERE job_code LIKE '03%'
+
+
+-- 6. Run a query to find the Effective Date (eff_date) or Salary End Date(sal_end_date) for grade Q90H0.
+
+SELECT
+    grade,
+    eff_date,
+    sal_end_date
+  FROM salary_range_by_job_classification
+  WHERE grade = 'Q90H0'
+
+
+-- 7. Sort the Biweekly low rate in ascending order.
+
+SELECT biweekly_low_rate
+  FROM salary_range_by_job_classification
+  ORDER BY biweekly_low_rate ASC
+
+-- Are these values properly sorted? 
+-- No, because this is a varchar field. 
+
+
+-- 8. What Step are Job Codes 0110-0400?
+
+SELECT 
+    Job_Code,
+    Salary_setID,
+    Step
+  FROM salary_range_by_job_classification
+  WHERE Job_Code BETWEEN "0110" AND "0400"
+  
+  
+-- 9. What is the Biweekly High Rate minus the Biweekly Low Rate for job Code 0170?  
+  
+SELECT
+    Job_code,
+    Biweekly_high_rate,
+    Biweekly_low_rate, 
+    (biweekly_high_rate - Biweekly_low_rate) AS calc
+  FROM salary_range_by_job_classification
+  WHERE Job_code = '0170'  
+  
+
+-- 10. What is the Extended Step for Pay Types M, H, and D?
+  
+SELECT Extended_step, Pay_Type
+  FROM salary_range_by_job_classification
+  WHERE Pay_Type IN ('M', 'H', 'D')
+
+
+-- 11. What is the step for Union Code 990 and a Set ID of SFMTA or COMMN?
+
+SELECT 
+    Union_code,
+    SetID,
+    Step
+  FROM salary_range_by_job_classification
+  WHERE 
+    Union_code = 990 
+    AND 
+    (SetID = 'SFMTA' OR SetID = 'COMMN')
+
+
