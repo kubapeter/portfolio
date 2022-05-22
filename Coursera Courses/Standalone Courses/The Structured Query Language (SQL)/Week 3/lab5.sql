c1db17986f9345fd8b965b5df3c5574194c55fee
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-22 20:21:27 +0200

    Update lab5.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab5.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 3/lab5.sql
@@ -1,19 +1,25 @@
 --2.1 List the productid, productname, unitprice 
 --of the lowest priced product Northwinds sells.   
 
-SELECT productid, productname, unitprice
+SELECT 
+    productid, 
+    productname, 
+    unitprice
   FROM "alanparadise/nw"."products"
   WHERE unitprice = 
     (
-      SELECT MIN(unitprice)
-      FROM "alanparadise/nw"."products"
-    )
+    SELECT MIN(unitprice)
+    FROM "alanparadise/nw"."products"
+    );
 
 --actually does not need subquery:
 
-SELECT productid, productname, unitprice
+SELECT 
+    productid, 
+    productname, 
+    unitprice
   FROM "alanparadise/nw"."products"
-  ORDER BY unitprice LIMIT 1
+  ORDER BY unitprice LIMIT 1;
 
 --2.2 How many orders in the orders table have a bad customerID 
 --(either missing or not on file in the customers table.)     
@@ -22,16 +28,17 @@ SELECT COUNT(orderid)
   FROM "alanparadise/nw"."orders"
   WHERE customerid NOT IN 
     (
-      SELECT customerID 
-        FROM "alanparadise/nw"."customers"
-    )
+    SELECT customerID 
+      FROM "alanparadise/nw"."customers"
+    );
 
 --2.3 Use a subquery in a SELECT to list something interesting.  
 
-SELECT productname, 
+SELECT 
+    productname, 
     (
-      SELECT to_char(sum(Unitprice*Unitsinstock),'999,999.99') AS TotalValue 
-        FROM "alanparadise/nw"."products"
+    SELECT to_char(sum(Unitprice*Unitsinstock),'999,999.99') AS TotalValue 
+      FROM "alanparadise/nw"."products"
     )
   FROM "alanparadise/nw"."products";   
   
@@ -46,15 +53,15 @@ SELECT
     TRUNC(sum(discount) * 100) total_discounts
 FROM
   (
-      SELECT  orderid, (quantity*unitprice) total_cost, discount
+  SELECT  orderid, (quantity*unitprice) total_cost, discount
+    FROM "alanparadise/nw"."orderdetails"
+    WHERE discount IN 
+      (
+      SELECT discount 
         FROM "alanparadise/nw"."orderdetails"
-        WHERE discount IN 
-          (
-            SELECT discount 
-              FROM "alanparadise/nw"."orderdetails"
-              GROUP BY discount
-              ORDER BY 1 DESC LIMIT 2
-          )
+        GROUP BY discount
+        ORDER BY 1 DESC LIMIT 2
+      )
   ) AS a
 GROUP BY orderid
 ORDER BY 3 DESC;
