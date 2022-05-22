bde8def195440821c9ebc7b6b92a312c17d848e0
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-22 20:17:07 +0200

    Update lab3.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab3.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab3.sql
@@ -1,15 +1,19 @@
 --2.1 List the employeeid, firstname + lastname concatenated as ‘employee’, 
 --and the age of the employee when they were hired. 
 
-SELECT employeeid, firstname || ' ' || lastname AS "employee",
+SELECT 
+    employeeid, 
+    firstname || ' ' || lastname AS "employee",
     age(hiredate, birthdate)::text
-  FROM "alanparadise/nw"."employees"
+  FROM "alanparadise/nw"."employees";
 
 --or
 
-SELECT EmployeeID, Firstname || ' ' || Lastname as "employee",
-		cast (age(HireDate, BirthDate)as text) AS HIRE_AGE 
-   	FROM "alanparadise/nw"."employees";
+SELECT 
+    EmployeeID, 
+    Firstname || ' ' || Lastname AS "employee",
+    cast (age(HireDate, BirthDate)AS text) AS HIRE_AGE 
+  FROM "alanparadise/nw"."employees";
 
 --2.2 Run a query to calculate your age as of today.  
 
@@ -22,6 +26,9 @@ SELECT CAST(age(now(),'2010-09-08')AS text);
 --2.3 List the customerid, companyname and country 
 --for all customers whose region is NULL.    
 
-SELECT customerid, companyname, country 
+SELECT 
+    customerid, 
+    companyname, 
+    country 
   FROM "alanparadise/nw"."customers"
-  WHERE region IS NULL
+  WHERE region IS NULL;
