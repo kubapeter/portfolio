495e66e270f53d3852d7c2051e5a5bc9690043bb
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-22 20:15:37 +0200

    Update lab2.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab2.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 2/lab2.sql
@@ -1,9 +1,12 @@
 --2.1 List the customerid, companyname, and country 
 --for all customers NOT in the U.S.A.
 
-SELECT customerid, companyname, country 
+SELECT 
+    customerid, 
+    companyname, 
+    country 
   FROM "alanparadise/nw"."customers"
-  WHERE country != 'USA'
+  WHERE country != 'USA';
 
 --2.2 For all products in the Northwinds database, 
 --list the productname, unitprice, unitsinstock, 
@@ -11,31 +14,43 @@ SELECT customerid, companyname, country
 --for all products with a Total Value greater than $100.  
 --(HINT:  total value = unitsinstock * unitprice) 
 
-SELECT productname, unitprice, unitsinstock, 
-	(unitprice * unitsinstock) AS "Total Value" 
-	  FROM "alanparadise/nw"."products"
-      WHERE (unitprice * unitsinstock) > 100
+SELECT 
+    productname, 
+    unitprice, 
+    unitsinstock, 
+    (unitprice * unitsinstock) AS "Total Value" 
+  FROM "alanparadise/nw"."products"
+  WHERE (unitprice * unitsinstock) > 100;
 
 --2.3 List the productid, productname, and quantityperunit 
 --for all products that come in bottles.   
 
-SELECT productid, productname, quantityperunit
-	FROM "alanparadise/nw"."products"
-  WHERE quantityperunit LIKE '%bottle%'
+SELECT 
+    productid, 
+    productname, 
+    quantityperunit
+  FROM "alanparadise/nw"."products"
+  WHERE quantityperunit LIKE '%bottle%';
 
 --2.4 List the productid, productname, and unitprice 
 --for all products whose categoryid is an ODD number.   
 --(HINT:  categoryid is a one digit integer less than 9 …) 
 
-SELECT productid, productname, unitprice 
-	FROM "alanparadise/nw"."products"
-  WHERE categoryid IN (1, 3, 5, 7)
+SELECT 
+    productid, 
+    productname, 
+    unitprice 
+  FROM "alanparadise/nw"."products"
+  WHERE categoryid IN (1, 3, 5, 7);
 
 --2.5 List the orderid, customerid, and shippeddate 
 --for orders that shipped to Canada 
 --in December 1996 through the end of  January 1997.
   
-SELECT orderid, customerid, shippeddate
-	FROM "alanparadise/nw"."orders"
-	WHERE shipcountry = 'Canada'
-		AND shippeddate BETWEEN '1996-12-01' AND '1997-01-31';
+SELECT 
+    orderid, 
+    customerid, 
+    shippeddate
+  FROM "alanparadise/nw"."orders"
+  WHERE shipcountry = 'Canada'
+    AND shippeddate BETWEEN '1996-12-01' AND '1997-01-31';
