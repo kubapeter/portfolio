557e456daed4354de813668c1e0473b81b70c05c
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-22 20:22:48 +0200

    Update lab6.sql

--- Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 4/lab6.sql
+++ Coursera Courses/Standalone Courses/The Structured Query Language (SQL)/Week 4/lab6.sql
@@ -3,22 +3,22 @@
 SELECT O.orderid, SUM(unitprice * quantity) AS "Total Value"
   FROM 
     "alanparadise/nw"."orders" AS O 
-    JOIN
+      JOIN
     "alanparadise/nw"."orderdetails" AS OD
-    ON O.orderid = OD.orderid
+      ON O.orderid = OD.orderid
   WHERE shipcountry = 'France'
   GROUP BY O.orderid
-  ORDER BY 2 DESC
+  ORDER BY 2 DESC;
 
 --2.2 Create a Suppliers List showing Supplier CompanyName, and names of all the products sold by each supplier located in Japan.   
 
 SELECT Supp.supplierid, companyname, productname
   FROM 
     "alanparadise/nw"."products" AS Prod 
-    JOIN
+      JOIN
     "alanparadise/nw"."suppliers" AS Supp
-    ON Prod.supplierid = Supp.supplierid
-  WHERE country = 'Japan'
+      ON Prod.supplierid = Supp.supplierid
+  WHERE country = 'Japan';
 
 --2.3 Create a “Low Performers” list showing 
 --the employees who have less than $100,000 in total sales. 
@@ -29,11 +29,11 @@ SELECT Supp.supplierid, companyname, productname
 SELECT LastName, Firstname, SUM(unitprice * quantity) as "Total Sales"
   FROM 
     "alanparadise/nw"."employees" E 
-    JOIN
+      JOIN
     "alanparadise/nw"."orders" O 
-    ON E.employeeid  =  O.employeeid
-    JOIN 
+      ON E.employeeid  =  O.employeeid
+      JOIN 
     "alanparadise/nw"."orderdetails" D 
-    ON O.orderid  =  D.orderid
+      ON O.orderid  =  D.orderid
   GROUP BY LastName, FirstName
-    HAVING  sum(unitprice * quantity) < 100000
+    HAVING sum(unitprice * quantity) < 100000;
