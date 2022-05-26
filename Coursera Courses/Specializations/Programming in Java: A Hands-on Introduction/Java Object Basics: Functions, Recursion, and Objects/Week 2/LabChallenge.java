79c1355fd663540d3bb958dd4436f2ef83760fd0
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-26 11:44:09 +0200

    Update LabChallenge.java

--- Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Object Basics: Functions, Recursion, and Objects/Week 2/LabChallenge.java
+++ Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Object Basics: Functions, Recursion, and Objects/Week 2/LabChallenge.java
@@ -1,1 +1,22 @@
+public class LabChallenge {
+  public static void main(String[] args) {
 
+    //call recursive method below this line
+    
+    System.out.println(recursivePower(4, 5));
+    
+    //call recursive method above this line
+  }
+
+  //add method definitions below this line
+  
+  public static int recursivePower(int base, int exponent) {
+    if (exponent == 0) {
+      return 1;
+    } else {
+      return base * recursivePower(base, exponent - 1);
+    }
+  }
+  
+  //add method definitions above this line
+}
