a50cdacdd490e496092343bb1c3b682cd9900f2f
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-26 12:10:33 +0200

    Update LabChallenge.java

--- Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Object Basics: Functions, Recursion, and Objects/Week 4/LabChallenge.java
+++ Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Object Basics: Functions, Recursion, and Objects/Week 4/LabChallenge.java
@@ -1,3 +1,50 @@
+import java.util.ArrayList;
+
+//add class definitions below this line
+
+class Zoo {
+  int bigCats;
+  int primates;
+  int reptiles;
+  int birds;
+  
+  Zoo(int bc, int p, int r, int b) {
+    bigCats = bc;
+    primates = p;
+    reptiles = r;
+    birds = b;
+  }
+  
+  int totalAnimals() {
+    return bigCats+primates+reptiles+birds;
+  }
+  
+  int totalMammals() {
+    return bigCats+primates;
+  }
+  
+  String mostAnimals() {
+    int max = bigCats;
+    String name = "bigCats";
+    
+    if (primates > max) {
+      max = primates;
+      name = "primates";
+    }
+    if (reptiles > max) {
+      max = reptiles;
+      name = "reptiles";
+    }
+    if (birds > max) {
+      max = birds;
+      name = "birds";
+    }
+    return name;
+  }
+}
+
+//add class definitions above this line
+
 public class LabChallenge {  
   public static void main(String[] args) {
     
