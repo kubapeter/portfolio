6e645ab0e1302d1eee805d4f7318b262b0e471d7
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-26 10:25:00 +0200

    Create LabChallenge.java

---
+++ Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Basics: Selection and Iteration/Week 4/LabChallenge.java
@@ -0,0 +1,15 @@
+public class LabChallenge {
+  public static void main(String args[]) {
+    
+    //add code below this line
+
+    for (int x = 0; x < 3; x++) {
+      System.out.println("XOXOXOXOX");
+      for (int y = 0; y < 2; y++) {
+        System.out.println("OXO");
+      }
+    }
+
+    //add code above this line
+  }
+}
