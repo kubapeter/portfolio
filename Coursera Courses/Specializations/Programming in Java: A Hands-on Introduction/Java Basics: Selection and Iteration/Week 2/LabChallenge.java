54adc9f77b2a8a99813380212e2ead40292bde30
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-26 10:09:19 +0200

    Create LabChallenge.java

---
+++ Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Basics: Selection and Iteration/Week 2/LabChallenge.java
@@ -0,0 +1,10 @@
+public class LabChallenge {
+  public static void main(String args[]) {
+    
+    //add code below this line
+    
+    System.out.println((5 > 7) && (false || 1 > 9) || 4 != 5 && ! (2 <= 3));
+    
+    //add code above this line
+  }
+}
