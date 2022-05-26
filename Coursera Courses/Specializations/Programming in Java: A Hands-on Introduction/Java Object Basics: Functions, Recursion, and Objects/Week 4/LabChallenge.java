b3aaa703eaa752f8f9572f826556170f2354058a
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-26 12:09:22 +0200

    Create LabChallenge.java

---
+++ Coursera Courses/Specializations/Programming in Java: A Hands-on Introduction/Java Object Basics: Functions, Recursion, and Objects/Week 4/LabChallenge.java
@@ -0,0 +1,13 @@
+public class LabChallenge {  
+  public static void main(String[] args) {
+    
+    //add code below this line
+    
+    Zoo myZoo = new Zoo(10, 30, 90, 120);
+    System.out.println(myZoo.totalAnimals());
+    System.out.println(myZoo.totalMammals());
+    System.out.println(myZoo.mostAnimals());
+
+    //add code above this line
+  }
+}
