92dfe9e5e71d4353953580accaaec1bb91b5bd5f
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-05-31 12:00:31 +0200

    Update flexible_data_formats.sql

--- Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/Data Wrangling, Analysis and AB Testing with SQL/Week 1/Practice Exercises/flexible_data_formats.sql
+++ Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/Data Wrangling, Analysis and AB Testing with SQL/Week 1/Practice Exercises/flexible_data_formats.sql
@@ -5,9 +5,8 @@ SELECT *
   FROM dsv1069.events
   WHERE event_name = 'view_item'
 
--- 2. Write a query to format the view_item event into a table with the appropriate columns
--- (This replicates what we had in the slides, but it is missing a column)
 
+-- 2. 
 
 SELECT 
     event_id, 
@@ -15,10 +14,63 @@ SELECT
     user_id,
     platform,
     (
-    CASE WHEN parameter_name == 'viewed_user_id'
+    CASE WHEN parameter_name = 'item_id'
          THEN CAST(parameter_value AS INT)
          ELSE NULL
     END
-    ) AS viewed_user_id
+    ) AS item_id
   FROM dsv1069.events
-  WHERE event_name = 'view_user_profile'
+  WHERE event_name = 'view_item'
+  ORDER BY event_id
+
+
+-- 3. 
+
+SELECT 
+    event_id, 
+    event_time, 
+    user_id,
+    platform,
+    (
+    CASE WHEN parameter_name = 'item_id'
+         THEN CAST(parameter_value AS INT)
+         ELSE NULL
+    END
+    ) AS item_id,
+    (
+    CASE WHEN parameter_name = 'referrer'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS referrer
+  FROM dsv1069.events
+  WHERE event_name = 'view_item'
+  ORDER BY event_id
+
+
+-- Final query: 
+
+SELECT 
+    event_id, 
+    event_time, 
+    user_id,
+    platform,
+    MAX(
+    CASE WHEN parameter_name = 'item_id'
+         THEN CAST(parameter_value AS INT)
+         ELSE NULL
+    END
+    ) AS item_id,
+    MAX(
+    CASE WHEN parameter_name = 'referrer'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS referrer
+  FROM dsv1069.events
+  WHERE event_name = 'view_item'
+  GROUP BY 
+    event_id, 
+    event_time, 
+    user_id,
+    platform
