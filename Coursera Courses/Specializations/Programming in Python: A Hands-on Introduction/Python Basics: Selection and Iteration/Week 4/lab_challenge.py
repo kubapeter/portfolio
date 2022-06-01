f304d861b38096dceb4efa8e9d198541a4d371e0
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-01 10:05:32 +0200

    Update lab_challenge.py

--- Coursera Courses/Specializations/Programming in Python: A Hands-on Introduction/Python Basics: Selection and Iteration/Week 4/lab_challenge.py
+++ Coursera Courses/Specializations/Programming in Python: A Hands-on Introduction/Python Basics: Selection and Iteration/Week 4/lab_challenge.py
@@ -1,15 +1,7 @@
 for row in range(8):
-  if row % 2 == 0:
-    for column in range(8):
-      if column % 2 == 0:
-        print("X", end="")
-      else:
-        print("O", end="")
-    print()
-  else:
-    for column in range(8):
-      if column % 2 == 0:
-        print("O", end="")
-      else:
-        print("X", end="")
-    print()
+  for column in range(8):
+    if ((column + row) % 2 ) == 0:
+      print("X", end="")
+    else:
+      print("O", end="")
+  print()
