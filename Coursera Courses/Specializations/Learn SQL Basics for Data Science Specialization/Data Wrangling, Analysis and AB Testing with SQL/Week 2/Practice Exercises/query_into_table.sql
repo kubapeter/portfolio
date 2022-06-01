a0d49ef6e24af94e4b7037a0c120d7c5c931dcd1
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-01 23:11:59 +0200

    Update query_into_table.sql

--- Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/Data Wrangling, Analysis and AB Testing with SQL/Week 2/Practice Exercises/query_into_table.sql
+++ Coursera Courses/Specializations/Learn SQL Basics for Data Science Specialization/Data Wrangling, Analysis and AB Testing with SQL/Week 2/Practice Exercises/query_into_table.sql
@@ -1,1 +1,85 @@
+-- 1. Quick way to make a table: CREATE TABLE ... AS
 
+CREATE TABLE
+  view_item_events_1
+AS
+SELECT 
+    event_id, 
+    event_time, 
+    user_id,
+    platform,
+    MAX(
+    CASE WHEN parameter_name = 'item_id'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS item_id,
+    MAX(
+    CASE WHEN parameter_name = 'referrer'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS referrer
+  FROM dsv1069.events
+  WHERE event_name = 'view_item'
+  GROUP BY 
+    event_id, 
+    event_time, 
+    user_id,
+    platform
+  ORDER BY event_id
+  
+  
+-- We can check the created table
+
+-- General information
+DESCRIBE view_item_events_1;
+
+-- Closer look
+SELECT * FROM view_item_events_1 LIMIT 10; 
+-- We can see that NOT ALL DATA TYPES ARE GOOD FOR USE!
+
+-- Clear the table (Only drop those tables that are yours)
+DROP TABLE view_item_events_1;
+
+
+-- 2. Now, not so quick table creation but the types are dealt with
+
+CREATE TABLE IF NOT EXISTS 'view_item_events' 
+  (
+  event_id   VARCHAR(32) NOT NULL PRIMARY KEY, 
+  event_time VARCHAR(26), 
+  user_id    INT(10), 
+  platform   VARCHAR(17), 
+  item_id    INT(10), 
+  referrer    VARCHAR(17)
+  );
+
+--INSERT INTO 'view_item_events' -- this would create duplicate rows
+REPLACE INTO 'view_item_events' -- this clears out the table first
+
+SELECT 
+    event_id, 
+    TIMESTAMP(event_time) AS event_time, 
+    user_id,
+    platform,
+    MAX(
+    CASE WHEN parameter_name = 'item_id'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS item_id,
+    MAX(
+    CASE WHEN parameter_name = 'referrer'
+         THEN parameter_value 
+         ELSE NULL
+    END
+    ) AS referrer
+  FROM dsv1069.events
+  WHERE event_name = 'view_item'
+  GROUP BY 
+    event_id, 
+    event_time, 
+    user_id,
+    platform
+  ORDER BY event_id
