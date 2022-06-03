67a5cac6d3324ffcc584cf8c3406134ec248aaec
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-03 10:17:33 +0200

    Update lab_challenge.py

--- Coursera Courses/Specializations/Programming in Python: A Hands-on Introduction/Python Object Basics: Functions, Recursion, and Objects/Week 3/lab_challenge.py
+++ Coursera Courses/Specializations/Programming in Python: A Hands-on Introduction/Python Object Basics: Functions, Recursion, and Objects/Week 3/lab_challenge.py
@@ -1,1 +1,14 @@
+#Use the class below to solve the lab challenge
+import copy
 
+class Dog:
+  def __init__(self, name, breed):
+    self.name = name
+    self.breed = breed
+    
+#Start your program below this line
+
+dog1 = Dog('Marceline', 'German Shepherd')
+dog2 = copy.deepcopy(dog1)
+dog2.name = 'Cajun'
+dog2.breed = 'Belgian Malinois'
