738dcd35209537132cd4850607dd720bd87c2ec1
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-04 21:20:33 +0200

    Update module3.sql

--- Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module3.sql
+++ Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module3.sql
@@ -1,7 +1,45 @@
 -- Setup
 
-CREATE TABLE Products
+CREATE TABLE CustomerInfo
 (
-  productID int,
-  productName varchar2(20)
+  custID int,
+  custName varchar2(20)
 );
+
+-- Let's take a look
+
+DESCRIBE CustomerInfo;
+
+-- Now create an index on custName (index will have the same name)
+
+CREATE INDEX custName
+  ON CustomerInfo(custName);
+  
+-- We see no change in the schema
+
+DESCRIBE CustomerInfo;
+
+-- To see the created index
+
+SELECT index_name
+  FROM all_indexes
+  WHERE table_name = 'CUSTOMERINFO';  -- all upper case is important
+  
+-- To see what column the created index is on
+
+SELECT index_name,
+       column_name
+  FROM all_ind_columns
+  WHERE table_name = 'CUSTOMERINFO'; 
+  
+-- Let's create another index for the custID
+
+CREATE INDEX custIDIndex
+  ON CustomerInfo(custID);
+  
+-- Now we can see the different column and index names
+
+SELECT index_name,
+       column_name
+  FROM all_ind_columns
+  WHERE table_name = 'CUSTOMERINFO'; 
