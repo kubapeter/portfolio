b7f44e45e40c8696cac66b881719799cced9fb2e
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-04 20:04:47 +0200

    Update module1.sql

--- Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module1.sql
+++ Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module1.sql
@@ -9,3 +9,35 @@ CREATE TABLE Products
 -- To see the schema
 
 DESCRIBE Products;
+
+-- Add column
+
+ALTER TABLE Products
+  ADD productDesc varchar2(50);
+  
+-- To see the change
+
+DESCRIBE Products;
+
+-- Modify column
+
+ALTER TABLE Products
+  MODIFY productDesc varchar2(40);
+  
+-- To see the change
+
+DESCRIBE Products;
+
+-- Delete a table
+
+DROP TABLE Products;
+
+-- To see the change
+
+DESCRIBE Products;
+-- we get an error message
+
+-- build table by executing a .sql file containing DDL commands
+
+@c:\data\buildTable.sql
+
