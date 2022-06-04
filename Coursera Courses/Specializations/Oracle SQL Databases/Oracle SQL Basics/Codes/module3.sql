4131d651cd74222aa34450c0bdf41b169c566fc1
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-04 21:40:10 +0200

    Update module3.sql

--- Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module3.sql
+++ Coursera Courses/Specializations/Oracle SQL Databases/Oracle SQL Basics/Codes/module3.sql
@@ -43,3 +43,55 @@ SELECT index_name,
        column_name
   FROM all_ind_columns
   WHERE table_name = 'CUSTOMERINFO'; 
+
+
+-- Setup
+
+CREATE TABLE UserInfo
+(
+  userID int,
+  userName varchar2(20)
+);
+
+-- There are no indexes. See what happens at a where clause
+
+EXPLAIN PLAN FOR
+SELECT userID,
+       userName
+  FROM UserInfo
+  WHERE userName = 'adam';
+  
+SELECT * 
+  FROM TABLE(dbms_xplan.display);
+-- We can see that there will be a full search (TABLE ACCESS FULL)
+
+-- What if we had an index? 
+
+CREATE INDEX userName
+  ON CustomerInfo(userName);
+  
+SELECT userID,
+       userName
+  FROM UserInfo
+  WHERE userName = 'adam';
+  
+SELECT * 
+  FROM TABLE(dbms_xplan.display);
+-- Now the scan will not go through the whole table
+
+-- Setup
+
+DROP TABLE UserInfo;
+
+CREATE TABLE UserInfo
+(
+  id int,
+  last_name varchar(20),
+  first_name varchar(20),
+  gender char(1)
+);
+
+-- Create a composite index
+
+CREATE INDEX index_full_name
+  ON UserInfo (last_name, first_name);
