6caf4f4106c01e16c90a39bfbd76f5a9b3995a97
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-05 16:57:17 +0200

    Create lab_challenge.py

---
+++ Coursera Courses/Specializations/Programming in Python: A Hands-on Introduction/Object-Oriented Python: Inheritance and Encapsulation/Week 4/lab_challenge.py
@@ -0,0 +1,7 @@
+from stats import Stats
+
+my_stats = Stats()
+
+print(my_stats.mean([8, 7, 3, 3, 1, 4, 9]))
+print(my_stats.median([8, 7, 3, 3, 1, 4, 9]))
+print(my_stats.mode([8, 7, 3, 3, 1, 4, 9]))
