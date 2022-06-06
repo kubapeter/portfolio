9ab69ed75a9f54fcbc2ac3b37f546d56364bf7cb
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-06 15:13:51 +0200

    Update exercise1.sql

--- Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/exercise1.sql
+++ Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/exercise1.sql
@@ -5,10 +5,49 @@ create table customers(
  custName varchar2(20) not null,
  empID varchar(10) not null
  );
-insert into customers (custCODE, custName, empID) values (3264,
-'TekNik', 'S2345');
-insert into customers (custCODE, custName, empID) values (1042,
-'KomTex', 'I4567');
-insert into customers (custCODE, custName, empID) values (6774,
-'Photon', 'S2345');
+ 
+insert into customers (custCODE, custName, empID) 
+  values (3264, 'TekNik', 'S2345');
+
+insert into customers (custCODE, custName, empID) 
+  values (1042, 'KomTex', 'I4567');
+
+insert into customers (custCODE, custName, empID) 
+  values (6774, 'Photon', 'S2345');
+
 select * from customers;
+
+-- Table 2: Create the Employees table
+
+create table employees(
+ empID varchar(10) not null,
+ empName varchar(20) not null,
+ role varchar(20)
+ );
+
+insert into employees (empID, empName) 
+  values ('S1234', 'Jack Peterson');
+insert into employees (empID, empName, Role) 
+  values ('M3456', 'Tom Hill', 'Marketing');
+insert into employees (empID, empName, Role) 
+  values ('I4567', 'Matt Abrams', 'Installation');
+
+select * from employees;
+
+-- Table 3: Create the Orders table
+
+create table orders(
+ orderNUM int not null,
+ custCODE varchar(20) not null,
+ empID varchar(10) not null,
+ orderDate date
+ );
+ 
+describe Orders;
+
+-- Table 4: Create the Vendors table
+
+create table vendors (
+ vendorID int not null );
+
+describe Vendors;
