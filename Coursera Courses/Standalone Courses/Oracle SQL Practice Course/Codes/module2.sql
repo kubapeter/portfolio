e3177e847d4e5621e97b630551fbcc361fef0fc9
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-06 14:03:24 +0200

    Update module2.sql

--- Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/module2.sql
+++ Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/module2.sql
@@ -13,3 +13,18 @@ CREATE TABLE emps
   empID int NOT NULL, 
   empName varchar(20) NOT NULL
 ); 
+
+-- Add a column
+
+ALTER TABLE emps
+  ADD email varchar(20);
+  
+-- Add constraint to a column
+
+ALTER TABLE emps
+  MODIFY email NOT NULL;
+
+-- Drop just one column 
+
+ALTER TABLE emps
+  DROP COLUMN email;
