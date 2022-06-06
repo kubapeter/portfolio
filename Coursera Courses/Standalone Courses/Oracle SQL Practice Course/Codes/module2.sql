cc62395e710a1aea4a68cf033eb667f5e4427755
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-06 14:00:19 +0200

    Update module2.sql

--- Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/module2.sql
+++ Coursera Courses/Standalone Courses/Oracle SQL Practice Course/Codes/module2.sql
@@ -1,1 +1,15 @@
+-- Create a table
 
+CREATE TABLE customer 
+(
+  customerID int, 
+  customerName varchar(20)
+); 
+
+-- Create a table with constraints
+
+CREATE TABLE emps 
+(
+  empID int NOT NULL, 
+  empName varchar(20) NOT NULL
+); 
