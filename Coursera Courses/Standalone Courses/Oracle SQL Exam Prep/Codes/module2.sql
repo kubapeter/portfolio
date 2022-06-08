db611e8909e6194d6adfbb9b68621b59f4b3f2f3
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-08 21:25:42 +0200

    Update module2.sql

--- Coursera Courses/Standalone Courses/Oracle SQL Exam Prep/Codes/module2.sql
+++ Coursera Courses/Standalone Courses/Oracle SQL Exam Prep/Codes/module2.sql
@@ -1,1 +1,29 @@
+-- ROLLBACK/COMMIT demonstration
 
+SELECT * 
+  FROM employees;
+  
+  -- we see the employees
+  
+INSERT INTO employees 
+  VALUES (4, 'John Williams', 3);
+  
+SELECT * 
+  FROM employees;
+  
+  -- we see the change
+  
+ROLLBACK;
+
+SELECT * 
+  FROM employees;
+  
+  -- now John Williams is not in the Table (was not COMMITted)
+
+
+INSERT INTO employees 
+  VALUES (4, 'Norah Jones', 3);
+  
+COMMIT; 
+
+  -- From now on, ROLLBACK only goes back here, the last COMMIT
