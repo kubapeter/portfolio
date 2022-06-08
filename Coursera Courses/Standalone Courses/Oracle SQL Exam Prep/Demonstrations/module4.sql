8fd367680d5f9a8e0d836450f478962d4005ebda
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-08 22:44:31 +0200

    Update module4.sql

--- Coursera Courses/Standalone Courses/Oracle SQL Exam Prep/Demonstrations/module4.sql
+++ Coursera Courses/Standalone Courses/Oracle SQL Exam Prep/Demonstrations/module4.sql
@@ -1,1 +1,45 @@
-CREATE VIEW
+-- VIEW demonstration
+
+CREATE VIEW vwEmpInfo AS
+  SELECT empName, 
+         deptName
+    FROM employees a, 
+         depts b
+    WHERE a.deptId = b.deptId;
+
+SELECT * 
+  FROM vwEmpInfo;
+  
+  -- changes can ce seen
+  
+INSERT INTO employees 
+  VALUES ('Lawrence Adams', 1)
+  
+SELECT * 
+  FROM vwEmpInfo;
+  
+  -- Lawrence can be seen in the view 
+  
+  
+-- MATERIALIZED VIEW demonstration  
+  
+CREATE MATERIALIZED VIEW mvEmpInfo AS
+  SELECT empName, 
+         deptName
+    FROM employees a, 
+         depts b
+    WHERE a.deptId = b.deptId;
+    
+SELECT * 
+  FROM mvEmpInfo;
+  
+  --looks the same as vwEmpInfo
+  
+INSERT INTO employees 
+  VALUES ('Amber Smith', 2)
+  
+SELECT * 
+  FROM mvEmpInfo;
+  
+  -- Amber is not seen on this. mv is a snapshot in time.
+  
