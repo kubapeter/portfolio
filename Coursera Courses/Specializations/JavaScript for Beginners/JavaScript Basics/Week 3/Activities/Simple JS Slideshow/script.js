90d90788698e072023fe7b46d48dd168f03a6a1c
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-08 13:11:33 +0200

    Create script.js

---
+++ Coursera Courses/Specializations/JavaScript for Beginners/JavaScript Basics/Week 3/Activities/Simple JS Slideshow/script.js
@@ -0,0 +1,30 @@
+(function(){
+  "use strict";
+
+  const myImages = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg'];
+  let currentImage = 0;
+  const next = document.getElementById('next');
+  const previous = document.getElementById('previous');
+
+  next.addEventListener('click', nextPhoto);
+  previous.addEventListener('click', previousPhoto);
+
+  function nextPhoto() {
+    currentImage++;
+    const img = document.getElementById('myimage');
+    if(currentImage > myImages.length-1){
+      currentImage = 0;
+    }
+    img.src = myImages[currentImage];
+  }
+
+  function previousPhoto() {
+    currentImage--;
+    const img = document.getElementById('myimage');
+    if(currentImage < 0){
+      currentImage = myImages.length-1;
+    }
+    img.src = myImages[currentImage];
+  }
+
+}());
