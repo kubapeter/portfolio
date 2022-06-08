ba7e75ebd36e09cc9ce158ddbb4b8bf81d9db521
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-08 15:35:57 +0200

    Update script.js

--- Coursera Courses/Specializations/JavaScript for Beginners/JavaScript Basics/Week 3/Activities/Slideshow with Cross Fade Effect/script.js
+++ Coursera Courses/Specializations/JavaScript for Beginners/JavaScript Basics/Week 3/Activities/Slideshow with Cross Fade Effect/script.js
@@ -5,19 +5,20 @@
   const myPhotos = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg'];
   
   const content = document.getElementById('content');
-  const next = document.getElementById('next');
-  const previous = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
+  const previousButton = document.getElementById('previous');
 
-  next.addEventListener('click', (e) =>{
-    e.preventDefault();
+  nextButton.addEventListener('click', (event) => {
+    event.preventDefault();
 
     currentImage++;
     if(currentImage>myPhotos.length-1){ currentImage = 0; }
 
     swapImage();
   });
-  previous.addEventListener('click', (e) => {
-    e.preventDefault();
+  
+  previousButton.addEventListener('click', (event) => {
+    event.preventDefault();
 
     currentImage--;
     if(currentImage<0){ currentImage = myPhotos.length-1; }
@@ -25,7 +26,7 @@
     swapImage();
   });
 
-  function swapImage(e){
+  function swapImage(event){
     const newSlide = document.createElement('img');
     newSlide.src = `slides/${myPhotos[currentImage]}`;
     newSlide.className = 'fadeinimg';
