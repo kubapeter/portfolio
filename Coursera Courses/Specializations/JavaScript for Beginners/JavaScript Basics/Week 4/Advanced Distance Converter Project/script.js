6ab35beb1dae333749fe9ba4e7e69bc912fbb5a8
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-09 11:41:11 +0200

    Update script.js

--- Coursera Courses/Specializations/JavaScript for Beginners/JavaScript Basics/Week 4/Advanced Distance Converter Project/script.js
+++ Coursera Courses/Specializations/JavaScript for Beginners/JavaScript Basics/Week 4/Advanced Distance Converter Project/script.js
@@ -15,11 +15,11 @@
     
     const distance = parseFloat(input.value);
     
-    if (distance && choiceConversion==='miles') {
+    if ( (distance || distance === 0) && choiceConversion === 'miles') {
       const conversion = (distance * 1.609344).toFixed(3);
       answer.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`;
     }
-    else if (distance && choiceConversion==='km') {
+    else if ( (distance || distance === 0) && choiceConversion === 'km') {
       const conversion = (distance/1.609344).toFixed(3);
       answer.innerHTML = `<h2>${distance} kilometers converts to ${conversion} miles</h2>`;
     }
@@ -30,13 +30,13 @@
   
   window.addEventListener('keydown', (event) => {
 
-    if(event.key.toLowerCase() === 'k'){
+    if (event.key.toLowerCase() === 'k') {
       h1Tag.innerText = 'Kilometers to Miles Converter';
       pTag.innerText = 'Type in a number of kilometers and click the button to convert the distance to miles.';
       directions.innerText = 'Press the "M" key to switch to mile conversion.';
       choiceConversion = 'km';
     }
-    else if(event.key.toLowerCase() === 'm'){
+    else if (event.key.toLowerCase() === 'm') {
       h1Tag.innerText = 'Miles to Kilometers Converter';
       pTag.innerText = 'Type in a number of miles and click the button to convert the distance to kilometers.';
       directions.innerText = 'Press the "K" key to switch to kilometer conversion.';
