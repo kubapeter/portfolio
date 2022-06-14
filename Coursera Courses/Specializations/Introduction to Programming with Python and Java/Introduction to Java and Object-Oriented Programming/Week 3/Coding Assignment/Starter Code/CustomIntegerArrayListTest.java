a2a26c6dfa6fc4d2c4302d103804b05777b9cc5c
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-14 16:12:58 +0200

    Add files via upload

---
+++ Coursera Courses/Specializations/Introduction to Programming with Python and Java/Introduction to Java and Object-Oriented Programming/Week 3/Coding Assignment/Starter Code/CustomIntegerArrayListTest.java
@@ -0,0 +1,194 @@
+package integerarraylist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class CustomIntegerArrayListTest {
+
+	@Test
+	void testGetArrayList() {
+		
+		//create a new empty CustomIntegerArrayList
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(2);
+		arr1.add(0, 5);
+		arr1.add(1, 5);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(2);
+		lst1.add(0, 5);
+		lst1.add(1, 5);
+		assertEquals(lst1, arr1.getArrayList());
+
+		ArrayList<Integer> arr4Elements = new ArrayList<Integer>();
+		arr4Elements.add(100);
+		arr4Elements.add(200);
+		arr4Elements.add(500);
+		
+		//create a new CustomIntegerArrayList with the elements in the given ArrayList
+		CustomIntegerArrayList arr4 = new CustomIntegerArrayList(arr4Elements);
+
+		ArrayList<Integer> lst4 = new ArrayList<Integer>();
+		lst4.add(100);
+		lst4.add(200);
+		lst4.add(500);
+
+		assertEquals(lst4, arr4.getArrayList());
+		
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testGet() {
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(2);
+		arr1.add(0, 5);
+		arr1.add(1, 5);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(2);
+		lst1.add(0, 5);
+		lst1.add(1, 5);
+
+		assertEquals(lst1.get(0), arr1.get(0));
+		assertEquals(lst1.get(1), arr1.get(1));
+		assertEquals(lst1.get(2), arr1.get(2));
+
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testAddInt() {
+		
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(2);
+		arr1.add(3);
+		arr1.add(4);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(2);
+		lst1.add(3);
+		lst1.add(4);
+
+		assertEquals(lst1.get(0), arr1.get(0));
+		assertEquals(lst1.get(1), arr1.get(1));
+		assertEquals(lst1.get(2), arr1.get(2));
+
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testAddIntInt() {
+		
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(0, 2);
+		arr1.add(0, 3);
+		arr1.add(0, 4);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(0, 2);
+		lst1.add(0, 3);
+		lst1.add(0, 4);
+
+		assertEquals(lst1.get(0), arr1.get(0));
+		assertEquals(lst1.get(1), arr1.get(1));
+		assertEquals(lst1.get(2), arr1.get(2));
+
+		// TODO write at least 3 additional test cases 	
+	}
+
+	@Test
+	void testRemoveInt() {
+
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(0, 2);
+		arr1.add(0, 3);
+		arr1.add(0, 4);
+		arr1.remove(0);
+		arr1.remove(1);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(0, 2);
+		lst1.add(0, 3);
+		lst1.add(0, 4);
+		lst1.remove(0);
+		lst1.remove(1);
+		
+		assertEquals(lst1.get(0), arr1.get(0));
+
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testRemoveIntInt() {
+
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(0, 2);
+		arr1.add(0, 3);
+		arr1.add(0, 3);
+		arr1.add(0, 3);
+		arr1.add(0, 3);
+		arr1.add(0, 4);
+		arr1.remove(3, 3);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(0, 2);
+		lst1.add(0, 3);
+		lst1.add(0, 4);
+
+		assertEquals(lst1, arr1.getArrayList());
+
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testSpliceIntInt() {
+
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(2);
+		arr1.add(3);
+		arr1.add(4);
+		arr1.add(5);
+		arr1.add(6);
+		arr1.splice(3, 2);
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(2);
+		lst1.add(3);
+		lst1.add(4);
+
+		assertEquals(lst1.get(0), arr1.get(0));
+		assertEquals(lst1.get(1), arr1.get(1));
+		assertEquals(lst1.get(2), arr1.get(2));
+
+		// TODO write at least 3 additional test cases 
+	}
+
+	@Test
+	void testSpliceIntIntIntArray() {
+		
+		CustomIntegerArrayList arr1 = new CustomIntegerArrayList();
+		arr1.add(2);
+		arr1.add(3);
+		arr1.add(4);
+		arr1.add(5);
+		arr1.add(6);
+		arr1.splice(3, 1, new int[] { 4, 5 });
+		
+		ArrayList<Integer> lst1 = new ArrayList<Integer>();
+		lst1.add(2);
+		lst1.add(3);
+		lst1.add(4);
+		lst1.add(4);
+		lst1.add(5);
+		lst1.add(6);
+		
+		assertEquals(lst1, arr1.getArrayList());
+
+		// TODO write at least 3 additional test cases 
+	}
+
+}
