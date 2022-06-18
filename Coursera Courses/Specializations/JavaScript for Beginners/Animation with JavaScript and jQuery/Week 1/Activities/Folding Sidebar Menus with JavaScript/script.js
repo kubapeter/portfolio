f72d96e15d1b6e6503694fa526393b85e5b51465
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-18 14:47:35 +0200

    Update script.js

--- Coursera Courses/Specializations/JavaScript for Beginners/Animation with JavaScript and jQuery/Week 1/Activities/Folding Sidebar Menus with JavaScript/script.js
+++ Coursera Courses/Specializations/JavaScript for Beginners/Animation with JavaScript and jQuery/Week 1/Activities/Folding Sidebar Menus with JavaScript/script.js
@@ -2,11 +2,30 @@
     "use strict";
     
     function hideSubmenus() {
+        const submenus = document.querySelectorAll("ul li ul");
+        
+        for (let i = 0; i < submenus.length; i++) {
+            submenus[i].className = "hide-menu";
+        }
     }
 
     hideSubmenus();
  
     const menuLinks = document.querySelectorAll(".menuLink");
-
+    
+    for (let i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('click', function(event) {
+            event.preventDefault();
+            const thisMenu = this.parentNode.querySelector('ul');
+            if (thisMenu.classList.contains('hide-menu')) {
+                hideSubmenus();
+                thisMenu.className = "show-menu";
+            }
+            else {
+                thisMenu.className = "hide-menu";
+            }
+        
+        });
+    }
 
 }());
