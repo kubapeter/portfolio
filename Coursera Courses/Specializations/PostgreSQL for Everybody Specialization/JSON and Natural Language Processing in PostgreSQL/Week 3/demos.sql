a4c56ac6f486323c9f55c8f5287875f6038a1f39
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-24 14:53:34 +0200

    Delete demos.sql

--- Coursera Courses/Specializations/PostgreSQL for Everybody Specialization/JSON and Natural Language Processing in PostgreSQL/Week 3/demos.sql
+++
@@ -1,9 +0,0 @@
--- The program will do these two commands and insert some rows
-DROP TABLE IF EXISTS pythonfun CASCADE;
-CREATE TABLE pythonfun 
-  (id SERIAL, 
-   line TEXT);
-
--- To check the results, use psql and look at the pythonfun table
-SELECT * 
-  FROM pythonfun;
