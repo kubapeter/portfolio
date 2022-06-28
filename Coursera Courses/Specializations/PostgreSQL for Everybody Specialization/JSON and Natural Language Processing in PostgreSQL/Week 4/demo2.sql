43c8c897a55a36ee23fa23b77b8fc3e4ceb9c1cf
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-06-28 13:48:44 +0200

    Update demo2.sql

--- Coursera Courses/Specializations/PostgreSQL for Everybody Specialization/JSON and Natural Language Processing in PostgreSQL/Week 4/demo2.sql
+++ Coursera Courses/Specializations/PostgreSQL for Everybody Specialization/JSON and Natural Language Processing in PostgreSQL/Week 4/demo2.sql
@@ -1,7 +1,7 @@
 --- To restart the spider
 DROP TABLE IF EXISTS swapi CASCADE;
 
--- swapi.py creates this if it does not exist
+-- demo2.py creates this if it does not exist
 CREATE TABLE IF NOT EXISTS swapi
   (
     id SERIAL, 
@@ -12,10 +12,6 @@ CREATE TABLE IF NOT EXISTS swapi
     updated_at TIMESTAMPTZ
   );
 
-SELECT url, status FROM SWAPI where URL like '%film%';
-
-SELECT COUNT(url) FROM swapi;
-
 -- While load is happening :)
 SELECT url 
   FROM swapi 
@@ -25,65 +21,147 @@ SELECT url
   FROM swapi 
   WHERE status IS NULL;
 
--- Load completed...
-SELECT body->>'url' FROM swapi LIMIT 1;
 
-SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
-EXPLAIN SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
+-- Load completed...
+SELECT COUNT(url) 
+  FROM swapi;
+  -- 207 documents loaded
+    
+SELECT url, status 
+  FROM swapi 
+  WHERE url LIKE '%film%';
+
+
+SELECT body->>'url' 
+  FROM swapi 
+  LIMIT 1;
+
+SELECT body->>'url' 
+  FROM swapi 
+  WHERE body @> '{"director": "George Lucas"}';
+  
+EXPLAIN 
+  SELECT body->>'url' 
+    FROM swapi 
+    WHERE body @> '{"director": "George Lucas"}';
+    -- sequential scan
+
 
 -- Lets throw some race cars into the table to make sure PG finds indexes useful
 INSERT INTO swapi (body) 
-SELECT ('{ "type": "Neon", "series": "24 Hours of Lemons", "number": ' || generate_series(1000,5000) || '}')::jsonb;
+  SELECT ('{ "type": "Neon", "series": "24 Hours of Lemons", "number": ' || generate_series(1000,5000) || '}')::jsonb;
 
-CREATE INDEX swapi_gin ON swapi USING gin (body jsonb_path_ops);
-EXPLAIN SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
+CREATE INDEX swapi_gin 
+  ON swapi 
+  USING gin (body jsonb_path_ops);
+  
+EXPLAIN 
+  SELECT body->>'url' 
+    FROM swapi 
+    WHERE body @> '{"director": "George Lucas"}';
+    -- and now it uses the GIN
 
 -- Try this ... oops - not what we meant
-SELECT body->>'url' FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb);
+SELECT body->>'url' 
+  FROM swapi 
+  WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb);
 
 -- We can fix that NOT bit with a clever WHERE clause...
-SELECT body->>'url' FROM swapi WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
-EXPLAIN SELECT body->>'url' FROM swapi WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
+-- First step:
+SELECT body->>'url' 
+  FROM swapi 
+  WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
+  
+EXPLAIN 
+  SELECT body->>'url' 
+    FROM swapi 
+    WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
+    -- sequential scan
 
 -- We can do a sequential scan
-SELECT body->>'url' FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb) AND 
-body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
+SELECT body->>'url' 
+  FROM swapi 
+  WHERE 
+    NOT(body @> '{"director": "George Lucas"}'::jsonb) 
+      AND 
+    body->>'url' LIKE 'https://swapi.py4e.com/api/films/%';
 
-SELECT body->>'url', body->>'name' FROM swapi LIMIT 10;
 
-SELECT body->>'url', body->>'name' FROM swapi 
-WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/people/%' LIMIT 10;
 
-SELECT body->>'url', body->>'name' FROM swapi 
-WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/species/%' LIMIT 10;
+SELECT body->>'url', 
+       body->>'name' 
+  FROM swapi 
+  LIMIT 10;
+
+SELECT body->>'url', 
+       body->>'name' 
+  FROM swapi 
+  WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/people/%' 
+  LIMIT 10;
+
+SELECT body->>'url', 
+       body->>'name' 
+  FROM swapi 
+  WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/species/%' 
+  LIMIT 10;
 
 -- LIKE makes a sequential scan
-EXPLAIN SELECT body->>'url', body->>'name' FROM swapi 
-WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/species/%' LIMIT 10;
+EXPLAIN 
+  SELECT body->>'url', 
+         body->>'name' 
+    FROM swapi 
+    WHERE body->>'url' LIKE 'https://swapi.py4e.com/api/species/%' 
+    LIMIT 10;
 
 -- Lets augment the JSON and add a "type" field using Regular Expressions
-
--- https://stackoverflow.com/questions/13615760/add-element-to-json-object-in-postgres
-
-SELECT substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') FROM swapi LIMIT 1;
+SELECT substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') 
+  FROM swapi 
+  LIMIT 1;
 
 SELECT ('{"type": "' || substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') || '" }')
-FROM swapi LIMIT 1;
+  FROM swapi 
+  LIMIT 1;
+  
 SELECT ('{"type": "' || substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') || '" }')::jsonb
-FROM swapi LIMIT 1;
+  FROM swapi 
+  LIMIT 1;
 
 -- Merge new json back into the body
 SELECT body || ('{"type": "' || substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') || '" }')::jsonb
-FROM swapi LIMIT 1;
+  FROM swapi 
+  LIMIT 1;
 
 -- Add the type field to all the records
-UPDATE swapi SWT SET body = body || 
-('{"type": "' || substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') || '" }')::jsonb;
+UPDATE swapi SWT 
+  SET body = body || ('{"type": "' || substring(body->>'url', 'https://swapi.py4e.com/api/([a-z]+)/') || '" }')::jsonb;
 
-SELECT body->>'url', body->>'name' FROM swapi WHERE body @> '{"type": "species"}'  LIMIT 10;
+SELECT body->>'url', 
+       body->>'name' 
+  FROM swapi 
+  WHERE body @> '{"type": "species"}'  
+  LIMIT 10;
 
-EXPLAIN SELECT body->>'url', body->>'name' FROM swapi WHERE body @> '{"type": "species"}'  LIMIT 10;
-
+EXPLAIN 
+  SELECT body->>'url', 
+         body->>'name' 
+    FROM swapi 
+    WHERE body @> '{"type": "species"}'  
+    LIMIT 10;
+  -- Now: not sequential scan! Uses GIN.
+  
 -- The payoff
-SELECT url FROM swapi WHERE body @> '{"type": "films"}' AND NOT(body @> '{"director": "George Lucas"}');
-EXPLAIN SELECT url FROM swapi WHERE body @> '{"type": "films"}' AND NOT(body @> '{"director": "George Lucas"}');
+SELECT url 
+  FROM swapi 
+  WHERE 
+    body @> '{"type": "films"}' 
+      AND 
+    NOT(body @> '{"director": "George Lucas"}');
+    
+EXPLAIN 
+  SELECT url 
+    FROM swapi 
+    WHERE 
+      body @> '{"type": "films"}' 
+        AND 
+      NOT(body @> '{"director": "George Lucas"}');
+      -- Not sequential (GIN)
