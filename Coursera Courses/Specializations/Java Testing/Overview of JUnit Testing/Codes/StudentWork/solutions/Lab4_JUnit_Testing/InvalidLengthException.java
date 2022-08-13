5e5d269fe87290e32a7f78e813995b854e36ab10
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-08-13 13:10:58 +0200

    Update InvalidLengthException.java

--- Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab4_JUnit_Testing/InvalidLengthException.java
+++ Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab4_JUnit_Testing/InvalidLengthException.java
@@ -1,6 +1,6 @@
 package com.secure.secret;
 
-public class InvalidLengthException extends RuntimeException {
+public class InvalidLengthException extends Exception {
   private static final long serialVersionUID = 1L;
 
 }
