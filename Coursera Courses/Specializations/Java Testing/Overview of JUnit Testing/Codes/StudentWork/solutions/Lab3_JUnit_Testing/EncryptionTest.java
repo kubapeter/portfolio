d938bedb027f17c3c90053e76b80321e0e07d076
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-08-13 13:24:14 +0200

    Update EncryptionTest.java

--- Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab3_JUnit_Testing/EncryptionTest.java
+++ Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab3_JUnit_Testing/EncryptionTest.java
@@ -9,19 +9,18 @@ class EncryptionTest {
 	@Test
 	void testEncrypt() {
 		assertAll( () -> assertEquals("Khoor#Zroug", BasicEncryption.encrypt("Hello World")),
-		() ->  assertEquals("d", BasicEncryption.encrypt("a"))
-//                , () -> assertTrue(false)
-//                , () -> assertTrue(true)
-//                , () -> assertSame(11, 12)
+			   () ->  assertEquals("d", BasicEncryption.encrypt("a"))
+//               	 , () -> assertTrue(false)
+//               	 , () -> assertTrue(true)
+//               	 , () -> assertSame(11, 12)
              );
 	}
 
 	@Test
 	void testDecrypt() {
 		assertAll( () -> assertEquals("Hello World", BasicEncryption.decrypt("Khoor#Zroug")),
-             () ->  assertEquals("WrIte ThIs GRoSs", BasicEncryption.decrypt("ZuLwh#WkLv#JUrVv")),
-             () ->  assertNotEquals("d", BasicEncryption.decrypt("d"))
-             );
+             		   () ->  assertEquals("WrIte ThIs GRoSs", BasicEncryption.decrypt("ZuLwh#WkLv#JUrVv")),
+             		   () ->  assertNotEquals("d", BasicEncryption.decrypt("d")) );
 	}
 
 }
