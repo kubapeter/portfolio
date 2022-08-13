58d904bc01c836a9df10ef49116d77dbd915660f
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-08-13 13:15:14 +0200

    Create BasicEncryption.java

---
+++ Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab4_JUnit_Testing/BasicEncryption.java
@@ -0,0 +1,41 @@
+package com.secure.secret;
+
+public class BasicEncryption {
+
+	public static String encrypt(String stringIn) throws InvalidLengthException {
+    
+    if (stringIn.length() > 100 | stringIn.length() < 3) {
+      throw new InvalidLengthException();
+    }
+
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < stringIn.length(); i++) {
+			int k = stringIn.charAt(i);
+			k += 3;
+			char encrypted = (char) k;
+			sb.append(encrypted);
+		}
+			String finshed = sb.reverse().toString();
+		return finshed;
+	}
+
+	public static String decrypt(String stringIn) {
+
+		StringBuilder june = new StringBuilder("");
+
+		for (int i = 0; i < stringIn.length(); i++) {
+			int k = stringIn.charAt(i);
+			k -= 3;
+			char decrypted = (char) k;
+			june.append(decrypted);
+		}
+			String come = june.reverse().toString();
+		return come;
+	}
+	
+	public static String decryptWithoutSB(String stringIn) {
+		return null;
+	}
+	
+}
