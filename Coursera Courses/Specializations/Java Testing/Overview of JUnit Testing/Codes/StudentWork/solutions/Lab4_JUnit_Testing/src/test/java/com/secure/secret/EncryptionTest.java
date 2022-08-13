d58907954cffb79f968f33fac7dac8d7271157c8
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-08-13 13:29:29 +0200

    Update EncryptionTest.java

--- Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab4_JUnit_Testing/src/test/java/com/secure/secret/EncryptionTest.java
+++ Coursera Courses/Specializations/Java Testing/Overview of JUnit Testing/Codes/StudentWork/solutions/Lab4_JUnit_Testing/src/test/java/com/secure/secret/EncryptionTest.java
@@ -9,14 +9,37 @@ class EncryptionTest {
 	@Test
 	void testEncrypt() {
 		assertAll( () -> assertEquals("Khoor#Zroug", BasicEncryption.encrypt("Hello World")),
-			   () ->  assertEquals("Dssoh", BasicEncryption.encrypt("Apple")));
+			   () ->  assertEquals("Dssoh", BasicEncryption.encrypt("Apple")) );
 	}
 
 	@Test
 	void testDecrypt() {
 		assertAll( () -> assertEquals("Hello World", BasicEncryption.decrypt("Khoor#Zroug")),
              		   () ->  assertEquals("WrIte ThIs GRoSs", BasicEncryption.decrypt("ZuLwh#WkLv#JUrVv")),
-			   () ->  assertNotEquals("d", BasicEncryption.decrypt("d")));
+			   () ->  assertNotEquals("Apple", BasicEncryption.decrypt("Apple")) );
+	}
+	
+	@Test
+	void encryptExceptionTestShort() {
+		try {
+			BasicEncryption.encrypt("a");
+			assertTrue(false);
+		}
+		catch(InvalidLengthException e) {
+			assertTrue(e instanceof InvalidLengthException);
+		}
+	}
+	
+	
+	@Test
+	void encryptExceptionTestLong() {
+		try {
+			BasicEncryption.encrypt("This is a much too long line, really really really really really really really really really really really really really really really really really really really really really really really long.");
+			assertTrue(false);
+		}
+		catch(InvalidLengthException e) {
+			assertTrue(e instanceof InvalidLengthException);
+		}
 	}
 
 }
