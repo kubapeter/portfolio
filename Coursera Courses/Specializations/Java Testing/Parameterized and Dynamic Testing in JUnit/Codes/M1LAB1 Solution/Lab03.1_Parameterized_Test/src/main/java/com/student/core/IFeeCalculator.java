371488553177c8f1a7ec24f39cc3badceb62baf0
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-08-21 15:43:29 +0200

    Delete Coursera Courses/Specializations/Java Testing/Parameterized and Dynamic Testing in JUnit/Codes/M1LAB1 Solution/Lab03.1_Parameterized_Test directory

--- Coursera Courses/Specializations/Java Testing/Parameterized and Dynamic Testing in JUnit/Codes/M1LAB1 Solution/Lab03.1_Parameterized_Test/src/main/java/com/student/core/IFeeCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.student.core;
-
-public interface IFeeCalculator {
-	public double setFee( int feeType);
-	public double feesDue();
-	public void payFee( double fee);
-}
--- Coursera Courses/Specializations/Java Testing/Parameterized and Dynamic Testing in JUnit/Codes/M1LAB1 Solution/Lab03.1_Parameterized_Test/src/main/java/com/student/core/IFeeProvider.java
+++
@@ -1,7 +0,0 @@
-package com.student.core;
-
-public interface IFeeProvider {
-	public void setFee( double fee);
-	public double feesDue();
-	public void payFee( double fee);
-}
