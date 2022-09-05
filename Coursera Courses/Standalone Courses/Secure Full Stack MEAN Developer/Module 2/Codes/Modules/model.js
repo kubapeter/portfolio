d849c8211baa656365053e6e48a29281d0b75fc5
kubapeter <59486906+kubapeter@users.noreply.github.com>
2022-09-05 15:34:46 +0200

    Create model.js

---
+++ Coursera Courses/Standalone Courses/Secure Full Stack MEAN Developer/Module 2/Codes/Modules/model.js
@@ -0,0 +1,15 @@
+//require, exports, module, __dirname, __filename
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(module)
+
+let names = ['Jill', 'lynn', 'Mike']
+let scores = [100, 90, 80]
+
+module.exports.names = names
+exports.scores = scores
+exports.data = [1, 2, 3]
+
+// console.log(exports)
